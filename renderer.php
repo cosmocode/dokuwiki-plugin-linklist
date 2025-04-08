1c70c440c65f33c0c72b32a258956c4bf27ebce8
Andreas Gohr <andi@splitbrain.org>
2025-04-08 16:38:41 +0200

    initial checkin

---
+++ renderer.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * DokuWiki Plugin linklist (Renderer Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+class renderer_plugin_linklist extends Doku_Renderer
+{
+    protected $data = [];
+
+
+    /** @inheritDoc */
+    public function getFormat()
+    {
+        return 'linklist';
+    }
+
+    public function document_start()
+    {
+        $this->data = [
+            'internal' => [],
+            'external' => [],
+            'interwiki' => [],
+        ];
+    }
+
+    public function document_end()
+    {
+        $this->doc = json_encode($this->data);
+    }
+
+
+    public function reset()
+    {
+        $this->document_start();
+    }
+
+    public function internallink($link, $title = null)
+    {
+        if (is_array($title)) $title = null;
+
+        if (isset($this->data['internal'][$link])) {
+            return; // already added
+        }
+        $this->data['internal'][$link] = [$link, $title];
+    }
+
+    public function externallink($link, $title = null)
+    {
+        if (is_array($title)) $title = null;
+
+        if (isset($this->data['external'][$link])) {
+            return; // already added
+        }
+        $this->data['external'][$link] = [$link, $title];
+    }
+
+    public function interwikilink($link, $title, $wikiName, $wikiUri)
+    {
+        if (is_array($title)) $title = null;
+
+        if (isset($this->data['interwiki'][$link])) {
+            return; // already added
+        }
+        $this->data['interwiki'][$link] = [$link, $title, $wikiName, $wikiUri];
+    }
+}
---
+++ syntax.php
@@ -0,0 +1,118 @@
+<?php
+
+use dokuwiki\Extension\SyntaxPlugin;
+
+/**
+ * DokuWiki Plugin linklist (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+class syntax_plugin_linklist extends SyntaxPlugin
+{
+    /** @inheritDoc */
+    public function getType()
+    {
+        return 'substition';
+    }
+
+    /** @inheritDoc */
+    public function getPType()
+    {
+        return 'block';
+    }
+
+    /** @inheritDoc */
+    public function getSort()
+    {
+        return 155;
+    }
+
+    /** @inheritDoc */
+    public function connectTo($mode)
+    {
+        $this->Lexer->addSpecialPattern('\{\{linklist[^\}]*?\}\}', $mode, 'plugin_linklist');
+    }
+
+
+    /** @inheritDoc */
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
+        $match = substr($match, 11, -2);
+        $params = explode(' ', $match);
+        $id = array_shift($params);
+        $params = array_map('trim', $params);
+
+        $type = 'internal';
+        if (in_array('backlinks', $params)) {
+            $type = 'backlinks';
+        } elseif (in_array('external', $params)) {
+            $type = 'external';
+        } elseif (in_array('interwiki', $params)) {
+            $type = 'interwiki';
+        }
+
+
+        $data = [
+            'id' => $id,
+            'type' => $type,
+        ];
+
+        return $data;
+    }
+
+    /** @inheritDoc */
+    public function render($mode, Doku_Renderer $renderer, $data)
+    {
+        if($mode == 'linklist') return false;
+        if($mode == 'metadata') return false;
+
+        global $INFO;
+
+        if (!$data['id']) $data['id'] = $INFO['id'];
+        if(!page_exists($data['id'])) return true;
+
+        switch ($data['type']) {
+            case 'backlinks':
+                // backlinks from the index
+                $links = ft_backlinks($data['id'], true);
+                $links = array_map(fn($link) => [$link, null], $links);
+                break;
+            case 'internal':
+            case 'external':
+            case 'interwiki':
+                // all other links from our own renderer
+                $results = p_cached_output(wikiFN($data['id']), 'linklist');
+                $results = json_decode($results, true, JSON_THROW_ON_ERROR);
+                $links = $results[$data['type']];
+        }
+
+
+        $links = array_filter($links, fn($item) => !isHiddenPage($item));
+        if (!$links) return true;
+
+        $renderer->listu_open();
+        foreach ($links as $params) {
+            $renderer->listitem_open(1);
+            $renderer->listcontent_open();
+            switch($data['type']) {
+                case 'backlinks':
+                case 'internal':
+                    $renderer->internallink($params[0], $params[1]);
+                    break;
+                case 'external':
+                    $renderer->externallink($params[0], $params[1]);
+                    break;
+                case 'interwiki':
+                    $renderer->interwikilink($params[0], $params[1], $params[2], $params[3]);
+                    break;
+            }
+            $renderer->listcontent_close();
+            $renderer->listitem_close();
+        }
+        $renderer->listu_close();
+
+
+        return true;
+    }
+}
